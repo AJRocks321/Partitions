915c5a7f595c066230bd44c9ae28a50ef68ed249
AJRocks321 <ajaymehrarocksboom@protonmail.com>
2021-03-30 22:25:47 +0530

    I merged the file to a single file

--- Terms_PART2.py
+++ Terms_PART2.py
@@ -1,4 +1,24 @@
-from Terms_PART1 import *
+
+def term(t,n):
+    L=[1]
+    c=L[0]
+    p=1
+    q=1
+    def y(x):
+        return 2*x+1
+    for i in range(1,n):
+        if (i%2==0):
+            c=c+y(p)
+            L=L+[c]
+            p=p+1
+        else:
+            c=c+q
+            L=L+[c]
+            q=q+1
+    return L[t]
+    
+    
+    
 def Formain(X):
     
     I=[1,1,2]
@@ -30,3 +50,11 @@ def Formain(X):
     for i in range(3,X+1):
         I=I+[y(i)]
     print(I[X])
+    
+    
+    
+    
+    
+print("This program is made by Ajay Mehra, and I want to say that it took me 2 days for writing this code and it was fun writing this, This program is designed for finding number of partitions (search partitions Number Theory (Wikipedia)) for any numbers ")
+a=int(input("Type any number I will find answer for you"))
+Formain(a)
